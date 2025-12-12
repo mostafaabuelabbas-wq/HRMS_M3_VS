f06cff7c62b56ef1fe4cbe1e6be57f4f26dc3be4
mohamadkhallaf <mohamad.khallaf@student.giu-uni.de>
2025-12-12 17:02:54 +0200

    shift and attendace done

---
+++ src/HRMS_M3_VS/Areas/Attendance/Models/AttendanceLogDto.cs
@@ -0,0 +1,24 @@
+﻿namespace HRMS_M3_VS.Areas.Attendance.Models
+{
+    public class AttendanceLogDto
+    {
+        public int attendance_id { get; set; }
+        public DateTime? entry_time { get; set; }
+        public DateTime? exit_time { get; set; }
+        public string shift_name { get; set; } = "Unknown";
+
+        // Helper to show duration
+        public string duration
+        {
+            get
+            {
+                if (entry_time.HasValue && exit_time.HasValue)
+                {
+                    var span = exit_time.Value - entry_time.Value;
+                    return $"{span.Hours}h {span.Minutes}m";
+                }
+                return "-";
+            }
+        }
+    }
+}
---
+++ src/HRMS_M3_VS/Areas/Attendance/Models/RecordAttendanceDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRMS_M3_VS.Areas.Attendance.Models
+{
+    public class RecordAttendanceDto
+    {
+        [Required]
+        public int employee_id { get; set; }
+
+        [Required]
+        public int shift_id { get; set; }
+
+        [Required]
+        public TimeSpan entry_time { get; set; }
+
+        [Required]
+        public TimeSpan exit_time { get; set; }
+    }
+}
