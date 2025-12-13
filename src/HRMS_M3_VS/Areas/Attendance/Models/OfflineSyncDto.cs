92143a4595d1fb6aefe3870dd246c111f79591cc
mohamadkhallaf <mohamad.khallaf@student.giu-uni.de>
2025-12-13 21:32:54 +0200

    attendance component done

---
+++ src/HRMS_M3_VS/Areas/Attendance/Models/OfflineSyncDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRMS_M3_VS.Areas.Attendance.Models
+{
+    public class OfflineSyncDto
+    {
+        [Required]
+        public int employee_id { get; set; }
+
+        [Required]
+        public int device_id { get; set; }
+
+        [Required]
+        public DateTime clock_time { get; set; } = DateTime.Now;
+
+        [Required]
+        public string type { get; set; } = "IN"; // 'IN' or 'OUT'
+    }
+}
---
+++ src/HRMS_M3_VS/Areas/Attendance/Models/TeamAttendanceLogDto.cs
@@ -0,0 +1,19 @@
+﻿namespace HRMS_M3_VS.Areas.Employee.Models
+{
+    public class TeamAttendanceLogDto
+    {
+        public int attendance_id { get; set; }
+        public int employee_id { get; set; }
+        public string full_name { get; set; } // Matches SQL e.full_name
+        public int? shift_id { get; set; }
+
+        public DateTime? entry_time { get; set; }
+        public DateTime? exit_time { get; set; }
+
+        public int? duration { get; set; } // Matches SQL duration
+
+        public string login_method { get; set; }
+        public string logout_method { get; set; }
+        public int? exception_id { get; set; }
+    }
+}
